4e8b4d997cf36baf290ca024e9f5cef6782cb57f
FPEF / Consultor - Kleber Ravi Carvalho de Oliveira <Kleber.Oliveira@gov.cv>
2022-07-30 16:04:01 -0100

    First Steps in java Spring boot 3

---
+++ 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/cv/com/kleber/Starup.java
@@ -0,0 +1,13 @@
+package cv.com.kleber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Starup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Starup.class, args);
+	}
+
+}
---
+++ 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java-erudio/src/test/java/cv/com/kleber/StarupTests.java
@@ -0,0 +1,13 @@
+package cv.com.kleber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
